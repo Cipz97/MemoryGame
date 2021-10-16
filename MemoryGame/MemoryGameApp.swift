6745e9a8e1db0ae81542713841a0037256bf7dd9
Cipz <matteo.cipone@gmail.com>
2021-10-16 13:52:03 +0200

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Matteo Cipone on 16.10.21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
